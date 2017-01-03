a8fa69f33cb0ae8451f66d94717c256f93daddd2
dsrini94 <sendtosrini@outlook.com>
2017-01-03 17:28:58 +0530

    firstcommit

--- Reset.jsx
+++ Reset.jsx
@@ -1,11 +1,66 @@
 import React from 'react';
+import Request from 'superagent';
+import Design from './posterDesign.jsx';
+import ReactDOM from 'react-dom';
+import AppBar from 'material-ui/AppBar';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import {List, ListItem} from 'material-ui/List';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
+
+var movieInfo;
 class Reset extends React.Component {
+
+	constructor(props)
+	{
+		super(props);
+		this.handleClick = this.handleClick.bind(this);
+	    this.handleChange = this.handleChange.bind(this);
+		this.state = {textValue:'',array:[]};
+	}
+
+	handleChange(e)
+	{
+		this.setState({textValue:document.getElementById('movie').value});
+	}
+	handleClick()
+	{
+		var temp=[];
+		Request.get("http://www.omdbapi.com/?s="+this.state.textValue).set('Accept', 'application/json')
+		.end((err, res) => {
+			if (res.status===200) {
+				var searched = res.body.Search;
+
+				
+				searched.forEach(function(item)
+				{
+					console.log(item);
+					temp.push(<Card>
+						<CardHeader title={item.Title}/>
+						<CardText>
+      						Type : {item.Type}<br/><br/>
+      						Release Year :{item.Year}
+    					</CardText>
+							</Card>);
+				})
+				
+    		}
+     else{
+     	console.log("Not ok");
+        return false;
+      }
+      this.setState({array:temp});
+  });
+		}
    render() {
+
       return (
         <div>
-          Being Awesome never goes out of fashion
-	<img src="hello.jpg"></img>
+        	<AppBar title="Movies Infinity" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+        	<TextField hintText="Search" id="movie" onChange={this.handleChange} style={{width:"80%"}}/>
+        	<RaisedButton label="Search" secondary={true}  onClick={this.handleClick} style={{marginLeft:"1%"}}/>
+        	{this.state.array}
          </div>
       );
    }
--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+injectTapEventPlugin();
+ReactDOM.render(<MuiThemeProvider>
+	<App />
+	</MuiThemeProvider>, document.getElementById('app'));
