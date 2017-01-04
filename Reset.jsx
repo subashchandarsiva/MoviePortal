32dede639b465e8e6498b707c75229da7cc69b78
dsrini94 <sendtosrini@outlook.com>
2017-01-04 10:08:48 +0530

    pagination

--- Reset.jsx
+++ Reset.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import Request from 'superagent';
-import Design from './posterDesign.jsx';
 import ReactDOM from 'react-dom';
 import AppBar from 'material-ui/AppBar';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import {List, ListItem} from 'material-ui/List';
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 
-
 var movieInfo;
+var temp=2;
 class Reset extends React.Component {
 
 	constructor(props)
@@ -17,9 +15,24 @@ class Reset extends React.Component {
 		super(props);
 		this.handleClick = this.handleClick.bind(this);
 	    this.handleChange = this.handleChange.bind(this);
-		this.state = {textValue:'',array:[]};
+	    this.handleNext = this.handleNext.bind(this);
+	    this.handlePrev = this.handlePrev.bind(this);
+		this.state = {textValue:'',array:[],activePage: 15,page:1};
 	}
 
+	handleNext() 
+	{
+    	 temp++;
+    	 this.setState({page:temp});
+    	 this.handleClick();
+  	}
+  	handlePrev()
+  	{
+  		temp--;
+  		this.setState({page:temp});
+    	this.handleClick();
+
+  	}
 	handleChange(e)
 	{
 		this.setState({textValue:document.getElementById('movie').value});
@@ -27,17 +40,19 @@ class Reset extends React.Component {
 	handleClick()
 	{
 		var temp=[];
-		Request.get("http://www.omdbapi.com/?s="+this.state.textValue).set('Accept', 'application/json')
+		this.setState({array:[]});
+		Request.get("http://www.omdbapi.com/?s="+this.state.textValue+'&page=' +this.state.page).set('Accept', 'application/json')
 		.end((err, res) => {
 			if (res.status===200) {
-				var searched = res.body.Search;
-
-				
+				var searched = res.body.Search;				
 				searched.forEach(function(item)
 				{
 					console.log(item);
 					temp.push(<Card>
 						<CardHeader title={item.Title}/>
+						<CardMedia style={{width:"400px",marginLeft:"35%"}}>
+      							<img src={item.Poster} style={{height:"500px"}} />
+    					</CardMedia>
 						<CardText>
       						Type : {item.Type}<br/><br/>
       						Release Year :{item.Year}
@@ -58,9 +73,12 @@ class Reset extends React.Component {
       return (
         <div>
         	<AppBar title="Movies Infinity" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
-        	<TextField hintText="Search" id="movie" onChange={this.handleChange} style={{width:"80%"}}/>
+        	<RaisedButton label="Prev" secondary={true}  onClick={this.handlePrev} style={{marginRight:"1%"}}/>
+        	<TextField hintText="Search" id="movie" onChange={this.handleChange} style={{width:"70%"}}/>
         	<RaisedButton label="Search" secondary={true}  onClick={this.handleClick} style={{marginLeft:"1%"}}/>
+        	<RaisedButton label="Next" secondary={true}  onClick={this.handleNext} style={{marginLeft:"1%"}}/>
         	{this.state.array}
+        	
          </div>
       );
    }
